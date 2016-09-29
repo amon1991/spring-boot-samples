8964f6543711b5d7fc37d9d4796719a4421d251a
amon1991 <809590249@qq.com>
2016-09-29 15:48:28 +0800

    update demo:make form can send object

--- spring-boot-file-upload-with-ajax/src/main/java/netgloo/controllers/MainController.java
+++ spring-boot-file-upload-with-ajax/src/main/java/netgloo/controllers/MainController.java
@@ -1,20 +1,17 @@
 package netgloo.controllers;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.nio.file.Paths;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Paths;
 
 @Controller
 public class MainController {
@@ -45,9 +42,13 @@ public class MainController {
   @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
   @ResponseBody
   public ResponseEntity<?> uploadFile(
-      @RequestParam("uploadfile") MultipartFile uploadfile) {
+      @RequestParam("uploadfile") MultipartFile uploadfile,@ModelAttribute("taskInfo") TaskInfo taskInfo) {
     
     try {
+
+        System.out.println("####id:"+taskInfo.getId());
+        System.out.println("####taskname:"+taskInfo.getTaskname());
+
       // Get the filename and build the local file path
       String filename = uploadfile.getOriginalFilename();
       String directory = env.getProperty("netgloo.paths.uploadedFiles");
@@ -58,6 +59,7 @@ public class MainController {
           new BufferedOutputStream(new FileOutputStream(new File(filepath)));
       stream.write(uploadfile.getBytes());
       stream.close();
+
     }
     catch (Exception e) {
       System.out.println(e.getMessage());
---
+++ spring-boot-file-upload-with-ajax/src/main/java/netgloo/controllers/TaskInfo.java
@@ -0,0 +1,28 @@
+package netgloo.controllers;
+
+/**
+ * yaming.chen@siemens.com
+ * Created by chenyaming on 2016/9/29.
+ */
+public class TaskInfo {
+
+    private String id;
+    private String taskname;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTaskname() {
+        return taskname;
+    }
+
+    public void setTaskname(String taskname) {
+        this.taskname = taskname;
+    }
+
+}
